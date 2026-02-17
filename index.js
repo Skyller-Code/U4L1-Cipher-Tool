8ad15ac064ef245a5466d566c03a7069cd484f81
Skyller <smbaughman08@outlook.com>
2026-02-17 14:48:40 -0500

    adding the buttons and description

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*
-const text = document.getElementById("fname").value;
-console.log(text);
-*/
-
 
 const subBut = document.getElementById("submit-btn");
 subBut.addEventListener("click", function(){
@@ -10,18 +5,12 @@ subBut.addEventListener("click", function(){
     console.log(input.value);
 });
 
+function encode()
+{
+    console.log("run encode function")
+}
 
-/*
-// 1. Select the elements
-const inputEl = document.getElementById("name");
-const buttonEl = document.getElementById("submit-btn");
-const outputEl = document.getElementById("output");
-
-// 2. Add an event listener to the button
-buttonEl.addEventListener("click", function() {
-  // 3. Access the .value property and use it
-  const inputValue = inputEl.value;
-  outputEl.textContent = inputValue;
-  console.log("Input value:", inputValue);
-});
-*/
+function decode()
+{
+    console.log("run decode function")
+}
