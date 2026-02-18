82ca5db759f4739727c5d0cdc308b5ebd078c620
Skyller <smbaughman08@outlook.com>
2026-02-18 14:54:18 -0500

    worked a lot of the encode and decode

--- index.js
+++ index.js
@@ -5,12 +5,44 @@ subBut.addEventListener("click", function(){
     console.log(input.value);
 });
 
-function encode()
+//might need to validate first
+
+function encode(text, key)
 {
-    console.log("run encode function")
+    console.log(text)
+
+    encoded_text = "";
+    alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(let index = 0; index < text.length; index++)
+    {
+        console.log((alphabet.search(text.charAt(index)) + key))
+        console.log((alphabet.search(text.charAt(index)) + key) % 26)
+        //the % shifts the letter back to the beginning if it goes over 26
+        encoded_text += alphabet[(alphabet.search(text.charAt(index)) + key) % 26]
+    }
+    console.log(encoded_text)
+
 }
 
-function decode()
+function decode(text, key)
 {
-    console.log("run decode function")
+    console.log(text)
+
+    encoded_text = "";
+    alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(let index = 0; index < text.length; index++)
+    {
+        //NOTE: js doesn't handle negative indexing
+        //i think i need an if states doing, doing what i did in python, unless i gt a nagative number, then use + // last left off
+        
+        console.log((alphabet.search(text.charAt(index)) - key))
+        //console.log((alphabet.search(text.charAt(index)) - key) % 26)
+        //console.log((alphabet.search(text.charAt(index)) - key) + 26)
+        //the % should shift the letter to the end if it goes under 0
+        
+        //encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) % 26]
+        //encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) + 26]
+        console.log(encoded_text)
+    }
+    console.log(encoded_text)
 }
