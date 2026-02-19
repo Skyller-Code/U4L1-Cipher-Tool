6adc1f15a4adf3b1c9d6601abe5f1731bc91bc9d
Skyller <smbaughman08@outlook.com>
2026-02-19 12:48:33 -0500

    added to js and html

--- index.js
+++ index.js
@@ -1,48 +1,80 @@
-
+/*
 const subBut = document.getElementById("submit-btn");
 subBut.addEventListener("click", function(){
     const input = document.getElementById("name");
     console.log(input.value);
 });
+*/
+
+function getText()
+{
+    const subBut = document.getElementById("sub-but");
+    subBut.addEventListener("click", function(){
+        const input = document.getElementById("input");
+        console.log(input.value);
+    });
+    return input.value
+}
+
+function getKey()
+{
+    const keyBut = document.getElementById("key-but");
+    keyBut.addEventListener("click", function(){
+        const keyIn = document.getElementById("key");
+        console.log(keyIn.value);
+    });
+}
 
 //might need to validate first
 
-function encode(text, key)
+function encode(key)
 {
-    console.log(text)
+    //console.log(input.value);
 
-    encoded_text = "";
-    alphabet = "abcdefghijklmnopqrstuvwxyz";
-    for(let index = 0; index < text.length; index++)
+    text = getText();
+    console.log(text);
+
+    if(text == "")
     {
-        console.log((alphabet.search(text.charAt(index)) + key))
-        console.log((alphabet.search(text.charAt(index)) + key) % 26)
-        //the % shifts the letter back to the beginning if it goes over 26
-        encoded_text += alphabet[(alphabet.search(text.charAt(index)) + key) % 26]
+        console.log("nothing");
     }
-    console.log(encoded_text)
+    else
+    {
+        //text = input.value
+        console.log(text);
 
+        encoded_text = "";
+        alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for(let index = 0; index < text.length; index++)
+        {
+            console.log((alphabet.search(text.charAt(index)) + key));
+            console.log((alphabet.search(text.charAt(index)) + key) % 26);
+            //the % shifts the letter back to the beginning if it goes over 26
+            encoded_text += alphabet[(alphabet.search(text.charAt(index)) + key) % 26];
+        }
+        console.log(encoded_text);
+    }
 }
 
 function decode(text, key)
 {
-    console.log(text)
+    console.log(text);
 
     encoded_text = "";
     alphabet = "abcdefghijklmnopqrstuvwxyz";
     for(let index = 0; index < text.length; index++)
     {
-        //NOTE: js doesn't handle negative indexing
-        //i think i need an if states doing, doing what i did in python, unless i gt a nagative number, then use + // last left off
-        
         console.log((alphabet.search(text.charAt(index)) - key))
-        //console.log((alphabet.search(text.charAt(index)) - key) % 26)
-        //console.log((alphabet.search(text.charAt(index)) - key) + 26)
-        //the % should shift the letter to the end if it goes under 0
-        
-        //encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) % 26]
-        //encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) + 26]
-        console.log(encoded_text)
+        if((alphabet.search(text.charAt(index)) - key) >= 0)
+        {
+            encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) % 26];
+        }
+        else
+        {
+            encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) + 26];
+        }
+
+        console.log(encoded_text);
     }
-    console.log(encoded_text)
+    console.log(encoded_text);
 }
