cc6990c283bedeaf3fb64bd6ee5b136170398d14
Skyller <smbaughman08@outlook.com>
2026-02-20 12:26:04 -0500

    pasted over codio stuff

--- index.js
+++ index.js
@@ -1,116 +1,68 @@
-/*
-const subBut = document.getElementById("submit-btn");
-subBut.addEventListener("click", function(){
-    const input = document.getElementById("name");
-    console.log(input.value);
-});
-*/
-
-function getText()
-{
-    const subBut = document.getElementById("sub-but");
-    subBut.addEventListener("click", function(){
-        const input = document.getElementById("input");
-        console.log(input.value);
-    });
-    return input.value
-}
-
-function getKey()
-{
-    const keyBut = document.getElementById("key-but");
-    keyBut.addEventListener("click", function(){
-        const keyIn = document.getElementById("key");
-        console.log(keyIn.value);
-    });
-}
-
-//might need to validate first
-
 function encode()
 {
-    console.log(Number("A"))
-    //console.log(input.value);
-
-    //const text = getText();
     const text = document.getElementById("input").value.toLowerCase();
-    console.log(text);
-
     const key = Number(document.getElementById("key").value);
-    console.log(key);
-
-    if(text == "" || (key == "" || key < 1))
+    if(text != "" && (key != "" && key > 0))
     {
-        console.log("nothing");
-    }
-    else
-    {
-        //text = input.value
-        console.log(text);
-
         encoded_text = "";
         alphabet = "abcdefghijklmnopqrstuvwxyz";
         for(let index = 0; index < text.length; index++)
         {
-            if(alphabet.includes(text.charAt(index)))
+            if(alphabet.includes(text.charAt(index))) //checks for special letters
             {
-                console.log((alphabet.search(text.charAt(index)) + key));
-                console.log((alphabet.search(text.charAt(index)) + key) % 26);
                 //the % shifts the letter back to the beginning if it goes over 26
-                console.log(alphabet[(alphabet.search(text.charAt(index)) + key) % 26])
-                console.log(alphabet[(alphabet.search(text.charAt(index)) + key) % 26].toLowerCase())
-                encoded_text += alphabet[(alphabet.search(text.charAt(index)) + key) % 26].toLowerCase();
+                encoded_text += alphabet[(alphabet.search(text.charAt(index)) + key) % 26];
             }
             else
             {
-                console.log(text.charAt(index))
                 encoded_text += text.charAt(index)
             }
             
         }
-        console.log(encoded_text);
-
         const output = document.createElement("h1");
-        output.textContent = `Output: ${encoded_text}`;
+        output.textContent = `Encoded Output: ${encoded_text}`;
         const body = document.getElementsByTagName("body")[0];
+        if(body.children.length > 12) //removes the output if a button is pressed again
+        {
+            body.removeChild(body.lastChild)
+        }
         body.appendChild(output);
     }
 }
 
 function decode()
 {
-    const text = document.getElementById("input").value;
+    const text = document.getElementById("input").value.toLowerCase();
     const key = Number(document.getElementById("key").value);
-
-    if(text == "" || (key == "" || key < 1))
-    {
-        console.log("nothing");
-    }
-    else
+    if(text != "" && (key != "" && key > 0))
     {
         encoded_text = "";
         alphabet = "abcdefghijklmnopqrstuvwxyz";
         for(let index = 0; index < text.length; index++)
         {
-            console.log((alphabet.search(text.charAt(index)) - key))
-            if((alphabet.search(text.charAt(index)) - key) >= 0)
+            if(alphabet.includes(text.charAt(index))) //checks for special letters
             {
-                encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) % 26];
+                if((alphabet.search(text.charAt(index)) - key) >= 0)
+                {
+                    encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) % 26];
+                }
+                else
+                {
+                    encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) + 26];
+                }
             }
             else
             {
-                encoded_text += alphabet[(alphabet.search(text.charAt(index)) - key) + 26];
+                encoded_text += text.charAt(index)
             }
-
-            console.log(encoded_text);
         }
-        console.log(encoded_text);
-
         const output = document.createElement("h1");
-        output.textContent = `Output: ${encoded_text}`;
+        output.textContent = `Decoded Output: ${encoded_text}`;
         const body = document.getElementsByTagName("body")[0];
+        if(body.children.length > 12) //removes the output if a button is pressed again
+        {
+            body.removeChild(body.lastChild)
+        }
         body.appendChild(output);
     }
-
-    
 }
